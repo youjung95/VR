41601b1822b0c313125da1403a9833391d731588
youjung95 <youjung94111@gmail.com>
2024-10-17 15:10:46 +0900

    1

---
+++ data.js
@@ -0,0 +1,254 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "버진로드뷰",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6854361358262198,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.07025384264244572,
+          "pitch": -0.00031151284382602284,
+          "rotation": 12.566370614359176,
+          "target": "1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1641312017756977,
+          "pitch": 0.2930120683135762,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">결혼</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.13824064732400387,
+          "pitch": 0.5300797228377849,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">웨딩홀&nbsp;</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">버진로드 뷰&nbsp;</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "1--",
+      "name": "중앙뷰로 이동",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2559146732004205,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "2-1--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.44901493607292053,
+          "pitch": 0.09207581915082841,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">웨딩홀</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">양측 아팝나무 신전 조형물_분수대</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "2-1--",
+      "name": "하객뷰1 로 이동",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0036316840000072403,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "3-2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-2--",
+      "name": "하객뷰2 로 이동",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5734627393011866,
+          "pitch": 0,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "입구",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0674332542997913,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.17528076599998954,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "조경",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0007677352999788667,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
